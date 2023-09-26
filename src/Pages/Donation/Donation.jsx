5533ae38ca7022c1f826f0be8953105b569bdeeb
Azizul <arafinazizul@gmail.com>
2023-09-27 04:14:00 +0600

    some modification on donation.jsx file

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -45,7 +45,10 @@ const Donation = () => {
             
             {
                 <div className="flex justify-center">
-                    <button onClick={()=>setShowMore(!showMore)} className=" bg-blue-600 py-2 px-3 my-5 text-white font-semibold">See More</button>
+                    {
+                       getDonation.length > 4? <button onClick={()=>setShowMore(!showMore)} className=" bg-blue-600 py-2 px-3 my-5 text-white font-semibold">{showMore ?'See Less':'See More'}</button>:''
+
+                    }
                </div> 
             }
 
